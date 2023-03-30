a299db2f866af838004ed0f215ded2e36ac76a0c
Giuseppe420 <84846526+maliabadziukh@users.noreply.github.com>
2023-03-30 18:34:31 +0200

    Start and End Screens
    everything works except there is a bug when the player holding the flag dies and gets spawned again before the flag is picked up by another player - it teleports back to the player that was holding it before and the game ends

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -20,27 +20,58 @@ public class GameController : MonoBehaviour
     public GameObject B2;
     public bool flagHeld = false;
     public bool flagCaptured = false;
+    public GameObject startScreen;
+    public GameObject endScreenA;
+    public GameObject endScreenB;
 
     public GameObject playerWithFlag = null;
     public string winningTeam = null;
 
     void Start()
     {
+        startScreen.SetActive(true);
+
+
+    }
+
+
+    void Update()
+    {
+
+        if (flagCaptured == true)
+        {
+            EndGame(); 
+        }
+    }
+
+    public void StartGame()
+    {
+        startScreen.SetActive(false);
+        endScreenA.SetActive(false);
+        endScreenB.SetActive(false);
         flagInstance = Instantiate(flagPrefab, flagBase.transform.position, flagBase.transform.rotation);
         A1 = Instantiate(playerA1, baseA.transform.position, baseA.transform.rotation);
         A2 = Instantiate(playerA2, baseA.transform.position, baseA.transform.rotation);
         B1 = Instantiate(playerB1, baseB.transform.position, baseB.transform.rotation);
         B2 = Instantiate(playerB2, baseB.transform.position, baseB.transform.rotation);
-
     }
 
-    
-    void Update()
+    void EndGame()
     {
-        
-        if (flagCaptured ==true){
-            Debug.Log(winningTeam + " wins!");
+        Destroy(A1);
+        Destroy(A2);
+        Destroy(B1);
+        Destroy(B2);
+        Destroy(flagInstance);
+        if (winningTeam == "BaseA")
+        {
+            endScreenA.SetActive(true);
+        } else if (winningTeam == "BaseB")
+        {
+            endScreenB.SetActive(true);
         }
+        flagCaptured = false;
+        
     }
-    
+
 }
